d7456369980ea6e40f7c063566aefd506462eeec
FaustoFe <51035010+FaustoFe@users.noreply.github.com>
2020-10-17 12:27:15 -0300

    Poco y nada

--- src/Main/Login.java
+++ src/Main/Login.java
@@ -2,27 +2,21 @@ package Main;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 
-public class Login {
+public abstract class Login {
 
 	private Connection cnx;
-	
-	public static void main(String[] args) {
-		
-		
-		
-	}
 
-	public Admin conectar(String clave) {
-		conectar("admin", clave);
-		return new Admin(cnx);
+	public boolean conectarBD(String clave) {
+		return conectarBD("admin", clave);
 	}
 	
-	public Inspector conectar(String usuario, String clave) {
+	public boolean conectarBD(String usuario, String clave) {
+	
+		// Conexión con el servidor
 		
-		/*
-		 *	Conección con el servidor
-		 */
+		boolean resultado = true;
 		
 		java.sql.DriverManager.setLoginTimeout(10);
 		
@@ -37,9 +31,48 @@ public class Login {
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());
+			resultado = false;
 		}
-		return new Inspector(cnx);
+		return resultado;
 	}
 	
+	private void desconectarBD() {
+	      if (this.cnx != null)
+	      {
+	         try
+	         {
+	            this.cnx.close();
+	            this.cnx = null;
+	         }
+	         catch (SQLException ex)
+	         {
+	            System.out.println("SQLException: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("VendorError: " + ex.getErrorCode());
+	         }
+	      }
+	  }
+	
+	public HashMap<String, String> consultaSQL(String consulta){
+		try
+		{
+			// Se crea una sentencia jdbc para realizar la consulta
+			java.sql.Statement stmt = cnx.createStatement();
+			
+			// Se prepara el string SQL de la inserción
+			String sql = "INSERT INTO barcos (nombre_barco, id, capitan) " + "VALUES ('Bismark', 22, 'Ernst Lindeman')";
+			
+			// Se ejecuta la inserción
+			stmt.execute(sql);
+			// Se retornan los recursos utilizados cerrando la sentencia
+			stmt.close();
+		} catch (java.sql.SQLException ex) {
+			System.out.println("Mensaje: " + ex.getMessage()); // Mensaje retornado por MySQL
+			System.out.println("Código: " + ex.getErrorCode()); // Código de error de MySQL System.out.println("SQLState: " +
+			ex.getSQLState(); // Código de error del SQL standart
+		}
+
+		return null;
+	}
 	
 }
