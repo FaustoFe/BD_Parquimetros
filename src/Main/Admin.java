e2b70f6103deb1e933d7a955e4863ea3f19056fa
FaustoFe <51035010+FaustoFe@users.noreply.github.com>
2020-10-20 16:25:33 -0300

    Logica de Admin

--- src/Main/Admin.java
+++ src/Main/Admin.java
@@ -1,12 +1,95 @@
 package Main;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Admin {
 
 	private Connection cnx;
+	private GUI_Admin gui;
 	
-	public Admin(Connection cnx) {
+	public Admin(GUI_Admin gui, Connection cnx) {
 		this.cnx = cnx;
+		this.gui = gui;
 	}
+	
+	public void sentenciaSQL(String consulta){
+		
+		boolean isResultSet = false;
+		Statement stmt = null;
+		ResultSet rslt = null;
+		
+		try {
+			// Se crea una sentencia jdbc para realizar la consulta
+			stmt = cnx.createStatement();
+			
+			isResultSet = stmt.execute(consulta); 
+			if (isResultSet) { //La sentencia tiene ResultSet
+				rslt = stmt.getResultSet();
+				gui.refrescar(rslt);
+				rslt.close();
+			}
+			else {
+				
+			}
+			
+			/*
+			 * Actualizar lista de tablas (por si se borro alguna tabla), se podria limpiar la lista de atributos.
+			 */
+			
+			stmt.close();
+			
+		} catch (java.sql.SQLException ex) {
+			System.out.println("Mensaje: " + ex.getMessage()); // Mensaje retornado por MySQL
+			System.out.println("Código: " + ex.getErrorCode()); // Código de error de MySQL 
+			System.out.println("SQLState: " + ex.getSQLState()); // Código de error del SQL standart
+			/*
+			 * Mostrar mensaje de error.
+			 */
+		}
+
+	}
+	
+	public ArrayList<String> getTablas() {
+		
+		Statement stmt = null;
+		ResultSet rslt = null;
+		ArrayList<String> resultado = new ArrayList<String>();
+	    
+		stmt = cnx.createStatement();
+		rslt = stmt.executeQuery("Show tables");
+	    
+	    while(rslt.next()) {
+	    	resultado.add(rslt.getString(1));
+	    }
+	    
+	    rslt.close();
+	    stmt.close();
+	
+	    return resultado;
+	}
+	
+public ArrayList<String> getAtributos(String tabla) {
+		
+		Statement stmt = null;
+		ResultSet rslt = null;
+		ArrayList<String> resultado = new ArrayList<String>();
+	    
+		stmt = cnx.createStatement();
+		rslt = stmt.executeQuery("SELECT * FROM " + tabla);
+		ResultSetMetaData metadata = rslt.getMetaData();
+	    
+	    for(int i = 0; i < metadata.getColumnCount(); i++) {
+	    	resultado.add(metadata.getColumnName(i));
+	    }
+	    
+	    rslt.close();
+	    stmt.close();
+	
+	    return resultado;
+	}
+	
 	
 }
