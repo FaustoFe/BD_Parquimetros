91132ac03de0c48ebc32affe8ee72af77417dd85
GiannFrancoo <GiannFrancoo1@hotmail.com>
2020-10-24 00:21:02 -0300

    Admin andando! arrancamos inspector

--- src/Main/GUI_Admin.java
+++ src/Main/GUI_Admin.java
@@ -3,13 +3,17 @@ package Main;
 import java.awt.EventQueue;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import java.awt.BorderLayout;
+import java.awt.Component;
+
 import quick.dbtable.*;
 
 import javax.swing.DefaultListModel;
@@ -27,15 +31,19 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
 import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent; 
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder; 
 
 public class GUI_Admin {
 
 	private JFrame frame;
-	
-	private DBTable tabla;
-	
+
 	private Admin admin;
 	private JTextField txtSelectFrom;
 	private JLabel lblAtributos, lblTablas; 
@@ -43,8 +51,15 @@ public class GUI_Admin {
 	private JButton btnEjecutar;
 	
 	private DefaultListModel modeloLT,modeloLA;
+	private DefaultTableModel modeloTabla;
 		
+	private JTable TablaDatos;
+	private JScrollPane scrollPane;
+	
 	private final Connection conexion;
+	private JButton btnVolver;
+	
+	
 	
 	public GUI_Admin(Connection cnx) {
 		conexion = cnx;
@@ -64,73 +79,54 @@ public class GUI_Admin {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		tabla = new DBTable();		
-		//tabla.getTable().setFont(new Font("Dubai", Font.PLAIN, 12));
-		tabla.setBounds(70, 400, 284, -244);
-		//tabla.getTable().setBounds(1, -1, 399, 0);
-		tabla.setEditable(false);
-		tabla.setVisible(true);
-		frame.getContentPane().add(tabla, BorderLayout.CENTER); 
-		//tabla.setLayout(null);
+		txtSelectFrom = new JTextField();
+		txtSelectFrom.setText("SELECT * FROM conductores");
+		txtSelectFrom.setFont(new Font("Dubai", Font.PLAIN, 14));
+		txtSelectFrom.setBounds(20, 11, 689, 30);
+		frame.getContentPane().add(txtSelectFrom);
+		txtSelectFrom.setColumns(10);
+		
+		
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		scrollPane.setBounds(20, 113, 415, 309);
+		frame.getContentPane().add(scrollPane);
+		
+		TablaDatos = new JTable();
+		TablaDatos.setRowSelectionAllowed(false);
+		TablaDatos.setFont(new Font("Dubai", Font.PLAIN, 14));
+		TablaDatos.setEnabled(false);
+		TablaDatos.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		
+		modeloTabla = new DefaultTableModel();
+		TablaDatos.setModel(modeloTabla);
+		ejecutarSentencia(txtSelectFrom.getText());
+		scrollPane.setViewportView(TablaDatos);		
+		
 		
 		
 		btnEjecutar = new JButton("Ejecutar");
+		btnEjecutar.setFont(new Font("Dubai", Font.PLAIN, 12));
+		btnEjecutar.setBounds(20, 52, 89, 37);
 		btnEjecutar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			
-				Statement stmt;
-				try {
-					stmt = conexion.createStatement();
-					String sql = "SELECT legajo FROM accede";
-					ResultSet rs = stmt.executeQuery(sql);
-					tabla.refresh(rs);
-				} catch (SQLException e1) {	e1.printStackTrace();}
-
-		        
-
-
-		        
-		       
-		                 
-		        //actualiza el contenido de la tabla con los datos del resulset rs
-		         //tabla.refresh(rs);
-				
-				
-				
-				/*
-				ResultSet rs = admin.sentenciaSQL(txtSelectFrom.getText());
-				if (rs != null) {
-					try {
-						tabla.refresh(rs);
-						rs.close();
-					} catch (SQLException d) { d.printStackTrace(); }
-				}
-				
-				*/
+				ejecutarSentencia(txtSelectFrom.getText());
 				actualizarListaTablas();
 				modeloLA.removeAllElements();
 				
 			}
 		});
-		btnEjecutar.setFont(new Font("Dubai", Font.PLAIN, 12));
-		btnEjecutar.setBounds(335, 32, 89, 37);
 		frame.getContentPane().add(btnEjecutar);
 		
-		txtSelectFrom = new JTextField();
-		txtSelectFrom.setText("SELECT * FROM multa");
-		txtSelectFrom.setFont(new Font("Dubai", Font.PLAIN, 12));
-		txtSelectFrom.setBounds(10, 12, 315, 77);
-		frame.getContentPane().add(txtSelectFrom);
-		txtSelectFrom.setColumns(10);
+
 		
 		ListaTablas = new JList();
 		ListaTablas.setToolTipText("Lista de tablas");
 		ListaTablas.setFont(new Font("Dubai", Font.PLAIN, 12));
-		ListaTablas.setBounds(463, 70, 110, 352);
+		ListaTablas.setBounds(463, 113, 110, 309);		
 		modeloLT = new DefaultListModel();
 		ListaTablas.setModel(modeloLT);
-		frame.getContentPane().add(ListaTablas);
-		
 		MouseListener mouseListener = new MouseAdapter() {
 		    public void mouseClicked(MouseEvent e) {
 		        if (e.getClickCount() == 1) {
@@ -140,28 +136,91 @@ public class GUI_Admin {
 		    }
 		};
 		ListaTablas.addMouseListener(mouseListener);
+		frame.getContentPane().add(ListaTablas);
 		
 		
 		ListaAtributos = new JList();
 		ListaAtributos.setToolTipText("Lista de atributos de una tabla");
 		ListaAtributos.setFont(new Font("Dubai", Font.PLAIN, 12));
-		ListaAtributos.setBounds(599, 70, 110, 352);
+		ListaAtributos.setBounds(599, 113, 110, 309);
 		modeloLA = new DefaultListModel();
 		ListaAtributos.setModel(modeloLA);		
 		frame.getContentPane().add(ListaAtributos);
 		
 		lblTablas = new JLabel("Tablas");
 		lblTablas.setFont(new Font("Dubai", Font.PLAIN, 14));
-		lblTablas.setBounds(494, 43, 46, 14);
+		lblTablas.setBounds(495, 88, 46, 14);
 		frame.getContentPane().add(lblTablas);
 		
 		lblAtributos = new JLabel("Atributos");
 		lblAtributos.setFont(new Font("Dubai", Font.PLAIN, 14));
-		lblAtributos.setBounds(626, 43, 64, 14);
+		lblAtributos.setBounds(623, 88, 64, 14);
 		frame.getContentPane().add(lblAtributos); 
 		
+		btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//Volver al inicio;
+				
+			}
+		});
+		btnVolver.setToolTipText("Volver");
+		btnVolver.setFont(new Font("Dubai", Font.PLAIN, 12));
+		btnVolver.setBounds(10, 429, 89, 37);
+		frame.getContentPane().add(btnVolver);
 	}	
 	
+	
+	private void ejecutarSentencia(String sql) {
+		ResultSet rs = admin.sentenciaSQL(sql);
+		if (rs != null) {
+			try {
+				actualizarTabla(rs);
+				rs.close();	
+			} catch (SQLException ex) {
+				System.out.println("Mensaje: " + ex.getMessage()); // Mensaje retornado por MySQL
+				System.out.println("Código: " + ex.getErrorCode()); // Código de error de MySQL 
+				System.out.println("SQLState: " + ex.getSQLState()); // Código de error del SQL standart
+			}
+		}
+		else {
+			//Sino cambiamos modelo;
+			//modeloTabla.getDataVector().removeAllElements();
+			modeloTabla.setRowCount(0);
+			modeloTabla.fireTableDataChanged();
+		}
+	}
+	
+	
+	private void actualizarTabla(ResultSet rs) throws SQLException{
+		
+		DefaultTableModel dtm = (DefaultTableModel) TablaDatos.getModel();
+				
+	    ResultSetMetaData metaData = rs.getMetaData();
+
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    dtm.setDataVector(data, columnNames);
+	    
+	    //resizeColumnWidth(TablaDatos);
+	    
+	 }
 	
 	
 	
@@ -186,5 +245,59 @@ public class GUI_Admin {
 	}
 	
 	
-
+	public void resizeColumnWidth(JTable table) {
+	    final TableColumnModel columnModel = table.getColumnModel();
+	    for (int column = 0; column < table.getColumnCount(); column++) {
+	        int width = 30; // Min width
+	        for (int row = 0; row < table.getRowCount(); row++) {
+	            TableCellRenderer renderer = table.getCellRenderer(row, column);
+	            Component comp = table.prepareRenderer(renderer, row, column);
+	            width = Math.max(comp.getPreferredSize().width + 1 , width);
+	        }
+	        if(width > 200)
+	            width = 200;
+	        columnModel.getColumn(column).setPreferredWidth(width);
+	    }
+	}
+	
+	
 }
+
+
+
+
+/*
+		tabla = new DBTable();		
+		//tabla.getTable().setFont(new Font("Dubai", Font.PLAIN, 12));
+		tabla.setBounds(70, 400, 284, -244);
+		//tabla.getTable().setBounds(1, -1, 399, 0);
+		tabla.setEditable(false);
+		tabla.setVisible(true);
+		frame.getContentPane().add(tabla, BorderLayout.CENTER); 
+		//tabla.setLayout(null);
+		
+	Statement stmt;
+	try {
+		stmt = conexion.createStatement();
+		String sql = "SELECT legajo FROM accede";
+		ResultSet rs = stmt.executeQuery(sql);
+		tabla.refresh(rs);
+	} catch (SQLException e1) {	e1.printStackTrace();}
+	
+	
+	
+	------------------------------------------------
+	
+	         
+	//actualiza el contenido de la tabla con los datos del resulset rs
+	 //tabla.refresh(rs);
+	
+	ResultSet rs = admin.sentenciaSQL(txtSelectFrom.getText());
+	if (rs != null) {
+		try {
+			tabla.refresh(rs);
+			rs.close();
+		} catch (SQLException d) { d.printStackTrace(); }
+	}
+
+*/
--- src/Main/GUI_Inspector.java
+++ src/Main/GUI_Inspector.java
@@ -2,13 +2,31 @@ package Main;
 
 import java.sql.Connection;
 import javax.swing.JFrame;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import java.awt.Color;
 
 public class GUI_Inspector {
 
 	private JFrame frame;
 	
 	private Inspector inspector;
+	private JTextField txtPatente;
+	
+	private JLabel lblPatente, lblListaPatente, lblSeleccionarUbicacion, lbSeleccionarParquimetro; 
+	private JButton btnAddPatente, btnEliminarPatente;
+	private JList listaPatentes;
+	private JComboBox cbUbicaciones, cbParquimetros;
+	
+	private DefaultListModel modeloLista;
 
 
 	public GUI_Inspector(Connection cnx) {
@@ -20,12 +38,79 @@ public class GUI_Inspector {
 
 	private void inicializarGUI() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(new Color(102, 204, 255));
+		frame.getContentPane().setFont(new Font("Dubai", Font.PLAIN, 12));
+		frame.setFont(new Font("Dubai", Font.PLAIN, 12));
+		frame.setTitle("Tablero inspector");
+		frame.setBounds(100, 100, 611, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(147, 118, 89, 23);
-		frame.getContentPane().add(btnNewButton);
+		txtPatente = new JTextField();
+		txtPatente.setFont(new Font("Dubai", Font.PLAIN, 12));
+		txtPatente.setBounds(21, 32, 103, 20);
+		frame.getContentPane().add(txtPatente);
+		txtPatente.setColumns(10);
+		
+		lblPatente = new JLabel("Patente");
+		lblPatente.setFont(new Font("Dubai", Font.PLAIN, 12));
+		lblPatente.setBounds(50, 11, 46, 14);
+		frame.getContentPane().add(lblPatente);
+		
+		btnAddPatente = new JButton("Agregar patente");
+		btnAddPatente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//En construcción, mandar a lista;
+				
+				txtPatente.getText();
+			}
+		});
+		btnAddPatente.setToolTipText("Agregar patente");
+		btnAddPatente.setFont(new Font("Dubai", Font.PLAIN, 12));
+		btnAddPatente.setBounds(134, 32, 122, 20);
+		frame.getContentPane().add(btnAddPatente);
+		
+		lblListaPatente = new JLabel("Listas patentes");
+		lblListaPatente.setHorizontalAlignment(SwingConstants.CENTER);
+		lblListaPatente.setFont(new Font("Dubai", Font.PLAIN, 12));
+		lblListaPatente.setBounds(21, 76, 103, 14);
+		frame.getContentPane().add(lblListaPatente);
+		
+		listaPatentes = new JList();
+		listaPatentes.setToolTipText("Lista patentes");
+		listaPatentes.setFont(new Font("Dubai", Font.PLAIN, 12));
+		listaPatentes.setBounds(21, 98, 103, 277);
+		modeloLista = new DefaultListModel();
+		frame.getContentPane().add(listaPatentes);
+		
+		lblSeleccionarUbicacion = new JLabel("Seleccionar ubicacion");
+		lblSeleccionarUbicacion.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSeleccionarUbicacion.setFont(new Font("Dubai", Font.PLAIN, 12));
+		lblSeleccionarUbicacion.setBounds(385, 11, 122, 14);
+		frame.getContentPane().add(lblSeleccionarUbicacion);
+		
+		cbUbicaciones = new JComboBox();
+		cbUbicaciones.setToolTipText("Ubicaciones");
+		cbUbicaciones.setFont(new Font("Dubai", Font.PLAIN, 12));
+		cbUbicaciones.setBounds(337, 31, 229, 21);
+		frame.getContentPane().add(cbUbicaciones);
+		
+		lbSeleccionarParquimetro = new JLabel("Seleccionar parquimetro");
+		lbSeleccionarParquimetro.setHorizontalAlignment(SwingConstants.CENTER);
+		lbSeleccionarParquimetro.setFont(new Font("Dubai", Font.PLAIN, 12));
+		lbSeleccionarParquimetro.setBounds(385, 172, 122, 14);
+		frame.getContentPane().add(lbSeleccionarParquimetro);
+		
+		cbParquimetros = new JComboBox();
+		cbParquimetros.setToolTipText("Parquimetros");
+		cbParquimetros.setFont(new Font("Dubai", Font.PLAIN, 12));
+		cbParquimetros.setBounds(337, 197, 229, 21);
+		frame.getContentPane().add(cbParquimetros);
+		
+		btnEliminarPatente = new JButton("Eliminar patente");
+		btnEliminarPatente.setToolTipText("Eliminar patente");
+		btnEliminarPatente.setFont(new Font("Dubai", Font.PLAIN, 12));
+		btnEliminarPatente.setBounds(134, 98, 122, 20);
+		frame.getContentPane().add(btnEliminarPatente);
 	}
 }
