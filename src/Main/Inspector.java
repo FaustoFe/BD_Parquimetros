fde4e85afcbaf7defc40174d2b3756fad2497112
FaustoFe <51035010+FaustoFe@users.noreply.github.com>
2020-10-24 21:02:08 -0300

    Update Inspector.java

--- src/Main/Inspector.java
+++ src/Main/Inspector.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Dictionary;
 
 public class Inspector {
 
@@ -23,7 +24,8 @@ public class Inspector {
 	/*
 	 * Retorna un ArrayList con las calles y alturas (ubicaciones) de todos los parquimetros.
 	 */
-	public ArrayList<String> getUbicaciones(){
+	public ArrayList<String> getParquimetros(){
+		//Dictionary<String, Pair<String, String>> resultado = new Dictionary<String, Pair<String, String>>();
 		ArrayList<String> resultado = new ArrayList<String>();
 		
 		try {
@@ -45,38 +47,14 @@ public class Inspector {
 		return resultado;
 	}
 	
-	/*
-	 * Retorna un ArrayList con todos los parquimetros de una ubicacion (calle, altura).
-	 */
-	public ArrayList<String> getParquimetros(){
-		ArrayList<String> resultado = new ArrayList<String>();
-		
-		try {
-			Statement stmt = cnx.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT numero FROM parquimetros");
-			
-			while(rs.next()) {
-				resultado.add(rs.getString("calle") + " " + rs.getString("altura"));
-			}
-		
-		
-		
-		} catch (java.sql.SQLException ex) {
-			System.out.println("Mensaje: " + ex.getMessage()); // Mensaje retornado por MySQL
-			System.out.println("Código: " + ex.getErrorCode()); // Código de error de MySQL 
-			System.out.println("SQLState: " + ex.getSQLState()); // Código de error del SQL standart
-		}
-		
-		return resultado;
-	}
-	
 	
 	/*
-	 * Retorna true si el inspector esta asociado a la ubicacion para la fecha y turno dado.
+	 * Retorna id_asociado_con del inspector que esta asociado a la ubicacion para la fecha y turno dado en caso de que exista.
+	 * Caso contrario retorna null.
 	 */
-	private boolean estaAsociado(Statement stmt, Fecha fecha, String calle, String altura) {
+	private String getAsociado(Statement stmt, Fecha fecha, String calle, String altura) {
 		
-		boolean resultado = false;
+		String resultado = null;
 		
 		String dia = fecha.getDia();
 		String turno = fecha.getTurno();
@@ -90,8 +68,8 @@ public class Inspector {
 									"AND calle = " + calle + "AND altura = " + altura + 
 									"AND legajo = " + legajo);
 		
-			if (rs.isBeforeFirst()) {
-				resultado = true;
+			if (rs.next()) {
+				resultado = rs.getString("id_asociado_con");
 			}
 			
 			rs.close();
@@ -113,8 +91,9 @@ public class Inspector {
 	 * y se registra el acceso del inspector al parquimetro.
 	 * Caso contrario muestra un mensaje notificando que el inspector no esta autorizado a labrar multas. 
 	 */
-	public void conectarParquimetro(String calle, String altura) {
+	public ArrayList<ArrayList<String>> conectarParquimetro(String calle, String altura) {
 		
+		ArrayList<ArrayList<String>> patentesMultadas = null;
 		Fecha fecha = new Fecha();
 		
 		// Controlar que el inspector tenga asociada la ubicacion correspondiente al parquimetro seleccionado, 
@@ -122,31 +101,73 @@ public class Inspector {
 		try {
 			Statement stmt = cnx.createStatement();
 			
-			if (estaAsociado(stmt, fecha, calle, altura)) { // Hay un inspector asociado a la ubicacion para el dia y turno actual.
+			String id_asociado = getAsociado(stmt, fecha, calle, altura);
+			
+			if (id_asociado != null) { // Hay un inspector asociado a la ubicacion para el dia y turno actual.
 				
 				ResultSet rs = stmt.executeQuery("SELECT patente FROM estacionados WHERE calle = " + calle + "AND altura = " + altura);
 				
+				String patente = null;
 				while(rs.next()){
-					patentesRegistradas.contains(rs.getString("patente"));
+					patente = rs.getString("patente");
+					if(patentesRegistradas.contains(patente)) {
+						patentesRegistradas.remove(patente);
+					}
+				}
+				rs.close();
+				
+				// Genera la lista de multas que se va a retornar, y se añade al batch (para insertarlas posteriormente a la base de datos)
+				patentesMultadas = new ArrayList<ArrayList<String>>();
+				if(!patentesRegistradas.isEmpty()) {
+					ResultSet rs_id = stmt.executeQuery("SELECT numero FROM multa ORDER BY numero ASC LIMIT 1");
+					rs_id.next();
+					int numeroMulta = Integer.parseInt(rs_id.getString("numero")) + 1;
+					rs_id.close();
+					
+					for(String p : patentesRegistradas) {
+						ArrayList<String> pMultada = new ArrayList<String>();
+						pMultada.add(String.valueOf(numeroMulta));
+						pMultada.add(String.valueOf(fecha.getDateSQL()));
+						pMultada.add(String.valueOf(fecha.getTimeSQL()));
+						pMultada.add(calle);
+						pMultada.add(altura);
+						pMultada.add(p);
+						pMultada.add(String.valueOf(legajo));
+						
+						patentesMultadas.add(pMultada);
+						
+						stmt.addBatch("INSERT INTO multa(numero, fecha, hora, patente, id_asociado_con) VALUES (" + numeroMulta + ", " + 
+										fecha.getDateSQL() + ", " + fecha.getTimeSQL() + ", " + p + ", " + id_asociado + ")");
+						
+						++numeroMulta;
+					}
 				}
 				
 				// Generar multas correspondientes
 				// Retornar un listado de las multas labradas con la siguiente informacion: numero de 
 				// multa, fecha, hora, calle, altura, patente del auto y legajo del inspector.
 				
+				
+				
 				// Registrar acceso del inspector al parquimetro
+				stmt.addBatch("INSERT INTO accede(legajo, id_parq, fecha, hora) VALUES (" + legajo + ", " + null + ", " + fecha.getDateSQL() + ", " + fecha.getTimeSQL() + ")");
+				
+				int[] b = stmt.executeBatch();
 			} 
 			else {
 				// De no ser asi, no se generaron las multas y se debera mostrar un mensaje indicando que el inspector no esta
 				// autorizado para labrar multas en esa ubicacion.
 			}
 		
+			stmt.close();
+			
 		} catch (java.sql.SQLException ex) {
 			System.out.println("Mensaje: " + ex.getMessage()); // Mensaje retornado por MySQL
 			System.out.println("Código: " + ex.getErrorCode()); // Código de error de MySQL 
 			System.out.println("SQLState: " + ex.getSQLState()); // Código de error del SQL standart
 		}
 		
+		return patentesMultadas;
 	}
 	
 	
